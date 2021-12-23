23b2bba71407957c3da5be579e92906b7464258b
theMahammad <hssjxie@gmail.com>
2021-12-23 20:17:34 +0400

    Updating Operation's been written

--- Program.cs
+++ Program.cs
@@ -21,6 +21,13 @@ namespace EntityFrameworkCore
            }
             
     }
+    static void AddProduct(Product product){
+        using(var db = new ShopContext()){
+            db.Products.Add(product);
+            db.SaveChanges();
+            Console.WriteLine("Data's been added!");
+        }
+            }
     static void ShowAllProducts(){
         using(var db = new ShopContext()){
             List<Product> Products= db.Products.ToList();
@@ -30,21 +37,46 @@ Console.WriteLine(WrittenProductsInDetail(product,db));
           
         }
     }
-    static void GetProductById(int id){
+    static Product GetProductById(int id){
         using(var db = new ShopContext() ){
             var selectedProduct = db.Products.Where(product => product.Id==id).FirstOrDefault(); // Thanks to FirstOrDefault, program don't throw a exception like First() method if a data had not found
             
-            Console.WriteLine(WrittenProductsInDetail(selectedProduct,db));
+           return selectedProduct;
         }
         
     }
+    static void UpdateProduct(Product product){
+        //Change tracking
+        using(var db = new ShopContext()){
+            var calledProduct = db.Products.Where(p => p.Id==product.Id).FirstOrDefault();
+            if (!(calledProduct is null)){
+                    calledProduct.Name = product.Name;
+                    calledProduct.Price = product.Price;
+                    calledProduct.CategoryId = product.CategoryId;
+                    
+                    
+                    db.SaveChanges();
+                    Console.WriteLine("Data's been updated");
+                  
+            } 
+        }
+            
+
+
+    }
+
 
     
         static void Main(string[] args)
         {
 
           ShowAllProducts();
-          GetProductById(1);
+         var product =GetProductById(1);
+         product.Name="Vestel Smart TV"; //Data's been updated
+         UpdateProduct(product);
+                   
+          
+          
 
     }
 }
